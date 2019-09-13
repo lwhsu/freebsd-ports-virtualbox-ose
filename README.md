8d10ad76c092f1210c9476d9d159142c812fb463
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-09-14 05:48:49 +0800

    Adopt Cirrus-CI

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://api.cirrus-ci.com/github/lwhsu/freebsd-ports-virtualbox-ose.svg)](https://cirrus-ci.com/github/lwhsu/freebsd-ports-virtualbox-ose)
---
+++ porttest.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+PORTSDIR=/usr/ports
+
+PORT=emulators/virtualbox-ose
+
+set -ex
+
+pwd
+
+cd /usr
+#ls -al ports
+mv ports ports.old
+#svnlite co svn://svn.freebsd.org/ports/head ports
+#portsnap --interactive fetch extract
+git clone --depth=1 https://github.com/freebsd/freebsd-ports ports
+
+cd ${CIRRUS_WORKING_DIR}
+for p in `cat list.txt`
+do
+	if [ -d ${PORTSDIR}/${p}/ ]; then
+		rm -fr ${PORTSDIR}/${p}/*
+	else
+		mkdir -p ${PORTSDIR}/${p}
+	fi
+        cp -R ./${p}/* ${PORTSDIR}/${p}/
+done
+
+mkdir /usr/ports/distfiles
+
+df -h
+
+echo "NO_ZFS=yes" >> /usr/local/etc/poudriere.conf
+echo "ALLOW_MAKE_JOBS=yes" >> /usr/local/etc/poudriere.conf
+sed -i.bak -e 's,FREEBSD_HOST=_PROTO_://_CHANGE_THIS_,FREEBSD_HOST=https://download.FreeBSD.org,' /usr/local/etc/poudriere.conf
+mkdir -p /usr/local/poudriere
+
+poudriere jail -c -j jail -v `uname -r`
+poudriere ports -c -f none -m null -M /usr/ports
+
+# use an easy port to bootstrap pkg repo
+poudriere bulk -t -j jail net/nc
+
+cd /usr/ports
+cd ${PORT}
+PWD=`pwd -P`
+PORTDIR=`dirname ${PWD}`
+PORTDIR=`dirname ${PORTDIR}`
+make all-depends-list | sed -e "s,${PORTDIR}/,," | xargs sudo pkg fetch -y -o pkgs
+rm -fr /usr/local/poudriere/data/packages/jail-default/.latest/All
+mv pkgs/All /usr/local/poudriere/data/packages/jail-default/.latest/
+rm -fr pkgs
+
+poudriere testport -j jail ${PORT}
